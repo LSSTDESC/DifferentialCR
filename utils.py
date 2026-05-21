8ca56172e70b1863165450867946468f314038a8
Maya Redden <msredden@stanford.edu>
2026-05-21 17:30:35 +0000

    updates

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def apply_filter(wvl, data, band = 'g', rm_leakage = True):
     '''
     wvl - a list of the wavelengths in nm
     data - a N x M list of SEDs, N = number of SEDs, M = len of wvl
-    returns data with the band filter applied to each SED
+    returns data with the band filter applied to each SEDwvl
     '''
     
     filter_file = f'filter_files/total_{band}.dat'
